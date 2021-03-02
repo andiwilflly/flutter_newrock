8f4e80a4f285fcb9364e5e68052d3caaf7b162d2
andiwillfly <andiwillfly@gmail.com>
2021-03-02 15:16:12 +0200

    nav

---
+++ lib/components/AppLayout.component.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+
+
+class AppLayout extends StatefulWidget {
+  @override
+  _AppLayoutState createState() => _AppLayoutState();
+}
+
+class _AppLayoutState extends State<AppLayout> {
+  int _page = 0;
+  GlobalKey _bottomNavigationKey = GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        bottomNavigationBar: CurvedNavigationBar(
+          key: _bottomNavigationKey,
+          index: 0,
+          height: 50.0,
+          items: <Widget>[
+            Icon(Icons.add, size: 20),
+            Icon(Icons.list, size: 20),
+            Icon(Icons.compare_arrows, size: 20),
+            Icon(Icons.call_split, size: 20),
+            Icon(Icons.perm_identity, size: 20),
+          ],
+          color: Colors.white,
+          buttonBackgroundColor: Colors.white,
+          backgroundColor: Colors.blueAccent,
+          animationCurve: Curves.easeInOut,
+          animationDuration: Duration(milliseconds: 300),
+          onTap: (index) {
+            setState(() {
+              _page = index;
+            });
+          },
+          letIndexChange: (index) => true,
+        ),
+        body: Container(
+          color: Colors.blueAccent,
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                Text(_page.toString(), textScaleFactor: 3.0),
+                RaisedButton(
+                  child: Text('Go To Page of index 1'),
+                  onPressed: () {
+                    final CurvedNavigationBarState navBarState =
+                        _bottomNavigationKey.currentState;
+                    navBarState.setPage(1);
+                  },
+                )
+              ],
+            ),
+          ),
+        ));
+  }
+}
